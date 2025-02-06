512eb64e920d1d5f6a491e4013fb8b220acdd7e4
Abdul Nayeem <154670886+abdul6966@users.noreply.github.com>
2025-02-06 11:56:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_X1n0V3QGm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3na65rhl9chhoqk2muc2ief00r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
